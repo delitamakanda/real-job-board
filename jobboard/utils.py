1542c640c29835a2faa7270cec7d27cfcdbf03f7
Délita Makanda <delitamakanda@users.noreply.github.com>
2019-12-24 14:23:54 +0100

    Create utils.py

---
+++ jobboard/utils.py
@@ -0,0 +1,9 @@
+
+def get_ip_address(request):
+    """ use requestobject to fetch client machine's IP Address """
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')    ### Real IP address of client Machine
+    return ip   
