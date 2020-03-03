7991a09c112e8032a7730e68379f88fa18f5e8e5
Délita Makanda <delita.makanda@gmail.com>
2020-03-03 23:30:24 +0100

    add react app

---
+++ frontend/build/precache-manifest.0717a92d8ac3f45c4e2dd4fa84107abf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12cf4df37a3959eb2aa1ca300100106e",
+    "url": "/static/frontend/build/index.html"
+  },
+  {
+    "revision": "cd82c6aa5dd82f289b25",
+    "url": "/static/frontend/build/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cd82c6aa5dd82f289b25",
+    "url": "/static/frontend/build/static/js/main.3b052fec.chunk.js"
+  },
+  {
+    "revision": "e21175813950e0152b3d",
+    "url": "/static/frontend/build/static/js/runtime-main.2dd748a6.js"
+  },
+  {
+    "revision": "7466b1ff444aa30bc2cd",
+    "url": "/static/frontend/build/static/js/vendors.995ec8de.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/frontend/build/static/js/vendors.995ec8de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/frontend/build/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ frontend/config-overrides.js
@@ -0,0 +1,31 @@
+var BundleTracker = require('webpack-bundle-tracker');
+var path = require('path');
+
+module.exports = {
+  webpack: (config, env) => {
+
+    config.optimization.splitChunks.name = 'vendors';
+
+    if (env === 'development') {
+      config.output.publicPath = 'http://localhost:3000/';
+    } else if (env === 'production') {
+      config.output.publicPath = '/static/frontend/build/';
+    }
+
+    config.plugins.push(
+      new BundleTracker({
+        path: __dirname,
+        filename: './webpack-stats.json',
+      }),
+    );
+    
+    return config;
+  },
+  devServer: function(configFunction) {
+    return function(proxy, allowedHost) {
+     const config = configFunction(proxy, allowedHost);
+     config.headers = {'Access-Control-Allow-Origin': '*'};
+     return config;
+    };
+  }
+};
