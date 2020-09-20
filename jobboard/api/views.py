d8f0ed1dbf01191506e59a9cf0a65acf635e6332
Délita Makanda <delita.makanda@gmail.com>
2020-09-21 01:29:52 +0200

    feat(search): add search functionnality

--- jobboard/api/views.py
+++ jobboard/api/views.py
@@ -1,5 +1,5 @@
-from rest_framework import viewsets
-from django_filters import rest_framework as filters
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 from jobboard.signals import object_viewed_signal
 from ..models import Annonce
@@ -11,6 +11,7 @@ from ..filters import AnnonceFilter
 class AnnonceViewSet(viewsets.ModelViewSet):
     queryset = Annonce.objects.all()
     serializer_class = AnnonceSerializer
-    filter_backends = ( filters.DjangoFilterBackend, )
+    filter_backends = ( DjangoFilterBackend, filters.SearchFilter, )
     filterset_class = AnnonceFilter
+    search_fields = ['^title', 'language', '^job_description', '^requirements', 'enterprise']
     lookup_field = 'slug'
